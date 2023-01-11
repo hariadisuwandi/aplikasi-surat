6211f4403249db058c7c13525707470a61066ec5
hariadisuwandi <hariadisuwandi2@gmail.com>
2023-01-11 13:17:30 +0800

    file baru

---
+++ admin/profil/data_profil.php
@@ -0,0 +1,54 @@
+<div class="card card-info">
+	<div class="card-header">
+		<h3 class="card-title">
+			<i class="fa fa-table"></i> Profil Kecamatan</h3>
+	</div>
+	<!-- /.card-header -->
+	<div class="card-body">
+		<div class="table-responsive">
+			<table id="" class="table table-bordered table-striped">
+				<thead>
+					<tr>
+						<th>Alamat</th>
+						<th>Kecamatan</th>
+						<th>Kabupaten</th>
+						<th>Kelola</th>
+					</tr>
+				</thead>
+				<tbody>
+
+					<?php
+              $no = 1;
+              $sql = $koneksi->query("select * from tb_profil");
+              while ($data= $sql->fetch_assoc()) {
+            ?>
+
+					<tr>
+						<td>
+						</td>
+						<td>
+							<?php echo $data['alamat']; ?>
+						</td>
+						<td>
+							<?php echo $data['kecamatan']; ?>
+						</td>
+						<td>
+							<?php echo $data['kabupaten']; ?>
+						</td>
+						<td>
+							<a href="?page=edit-profil&kode=<?php echo $data['id_profil']; ?>" title="Ubah"
+							 class="btn btn-success btn-sm">
+								<i class="fa fa-wrench"></i>
+							</a>
+						</td>
+					</tr>
+
+					<?php
+              }
+            ?>
+				</tbody>
+				</tfoot>
+			</table>
+		</div>
+	</div>
+	<!-- /.card-body -->
